b5aa647fd3b59a13db2ee70ad83fef4ffaea625d
root <georgerschmid@gmail.com>
2020-09-20 18:49:50 +0000

    add hello world program

---
+++ src/helloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ *  a simple java hello world program
+ * 
+ */
+
+public class helloWorld{
+
+    public static void main(String args[]){
+        //prints hello world
+        System.out.println("Hello World");
+    }
+
+ }
